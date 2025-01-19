7049eb92e79bf4ab3ca21c0e815c3f2aa05b6472
Samiksha <ikshasam09@gmail.com>
2025-01-19 13:16:49 +0530

    final commit

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -76,7 +76,7 @@ const Card = ({
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center
-       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl "
+       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl"
       style={{
         background: "rgb(4,7,29)",
         backgroundColor:
