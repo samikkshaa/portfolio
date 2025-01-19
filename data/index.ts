cbeea29f3cbab7b500714d1b8022f2101dec3845
Samiksha <ikshasam09@gmail.com>
2025-01-19 12:42:29 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,233 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I thrive on creating user-friendly interfaces and robust solutions",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building an AI Landing Page",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Recipe-Book App",
+    des: "Explore the recipes of all over the world especially international recipes which user belonging in India also would like to explore",
+    img: "https://media.istockphoto.com/id/1457979959/photo/snack-junk-fast-food-on-table-in-restaurant-soup-sauce-ornament-grill-hamburger-french-fries.jpg?s=612x612&w=0&k=20&c=QbFk2SfDb-7oK5Wo9dKmzFGNoi-h8HVEdOYWZbIjffo=",
+    iconLists: [
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM.jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(1).jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(2).jpeg",
+    ],
+    link: "http://127.0.0.1:3001/index.html",
+  },
+  {
+    id: 2,
+    title: "Basic Calculator",
+    des: "A calculator to calculate the basic values by addition, subtraction, multiplication and division and etc.",
+    img: "https://m.media-amazon.com/images/I/71mL4boUKwL._AC_UF1000,1000_QL80_.jpg",
+    iconLists: [
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM.jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(1).jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(2).jpeg",
+    ],
+    link: "http://127.0.0.1:3001/index.html",
+  },
+  {
+    id: 3,
+    title: "Todo List",
+    des: "A todo list in which user add items of todo work and delete as well which has been done by him/her.",
+    img: "https://t3.ftcdn.net/jpg/02/59/31/70/360_F_259317013_nJJaBgGGzvXMd6cAyLd6yMJtbdnd61wk.jpg",
+    iconLists: [
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM.jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(1).jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(2).jpeg",
+    ],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Heart-Trail Animation",
+    des: "whenever user hover on the screen, many hearts will pop up in the screen randomly",
+    img: "https://cdn.pixabay.com/photo/2018/01/22/01/08/heart-3097905_640.jpg",
+    iconLists: [
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM.jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(1).jpeg",
+      "WhatsApp%20Image%202025-01-18%20at%201.34.09%20PM%20(2).jpeg",
+    ],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collabrating with Samiksha as a hackathon team leader, it was a great pleasure and she was so supporting as well. she continously work with passion and didn't get tired of it. It makes me so motivated. I appreciate her and insist all the clients or collegues to collabrate with her.",
+    name: "Aryansh Rastogi",
+    title: "Teammate in college",
+  },
+  {
+    quote:
+      "Collabrating with Samiksha as a hackathon team leader, it was a great pleasure and she was so supporting as well. she continously work with passion and didn't get tired of it. It makes me so motivated. I appreciate her and insist all the clients or collegues to collabrate with her.",
+    name: "Shrishti Gupta",
+    title: "Teammate in college",
+  },
+  {
+    quote:
+      "Collabrating with Samiksha as a hackathon team leader, it was a great pleasure and she was so supporting as well. she continously work with passion and didn't get tired of it. It makes me so motivated. I appreciate her and insist all the clients or collegues to collabrate with her.",
+    name: "Shashank Suman",
+    title: "Teammate in college",
+  },
+  {
+    quote:
+      "Collabrating with Samiksha as a hackathon team leader, it was a great pleasure and she was so supporting as well. she continously work with passion and didn't get tired of it. It makes me so motivated. I appreciate her and insist all the clients or collegues to collabrate with her.",
+    name: "Abhijeet Pratap Singh",
+    title: "Teammate in college",
+  },
+  {
+    quote:
+      "Collabrating with Samiksha as a hackathon team leader, it was a great pleasure and she was so supporting as well. she continously work with passion and didn't get tired of it. It makes me so motivated. I appreciate her and insist all the clients or collegues to collabrate with her.",
+    name: "Saumya Shukla",
+    title: "Teammate in college",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Hackathon",
+    desc: "A Team leader in Hackathon organised in college",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "An engineer's day event",
+    desc: "A participant of an event named Web Wonder",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "An engineer's day event",
+    desc: "Participated in debugging",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Frontend intern in Avinya company",
+    desc: "Developed a frontend AI Landing page with react and next.js",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "https://cdn.iconscout.com/icon/free/png-256/free-leetcode-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-company-vol-1-pack-logos-icons-3030025.png",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
