9667a6b406ed08b486ceb00c664b79170068921a
Samiksha <ikshasam09@gmail.com>
2025-01-19 13:23:26 +0530

    last commit

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-import { companies, testimonials } from "@/data";
+import { testimonials } from "@/data";
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const Clients = () => {
@@ -24,7 +24,7 @@ const Clients = () => {
           />
         </div>
 
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+        {/* <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
@@ -39,10 +39,10 @@ const Clients = () => {
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
                   className="md:w-24 w-20"
                 />
-              </div>
-            </React.Fragment>
-          ))}
-        </div>
+              </div> */}
+            {/* </React.Fragment> */}
+          {/* ))} */}
+        {/* </div> */}
       </div>
     </section>
   );
