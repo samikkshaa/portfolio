2b821e7a9d5c33ad90fe73e69e2da80e8f320234
Samiksha <ikshasam09@gmail.com>
2025-01-20 08:04:46 +0530

    changed something

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I'm Samiksha, a React.js and Next.js Developer based in India.
+            Hi! I&apos;m Samiksha, a React.js and Next.js Developer based in India.
           </p>
 
           <a href="#about">
